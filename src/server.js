5436d21d1b0ce451d21dd83fe77deff010f803ad
minhlc1108 <minh.lcm.2003@gmail.com>
2024-03-29 00:34:53 +0700

    Init code

---
+++ src/server.js
@@ -0,0 +1,14 @@
+// const express = require('express')
+import express from 'express'
+const app = express()
+
+const hostname = 'localhost'
+const port = 8017
+
+app.get('/', (req, res) => {
+  res.send("<h1>Hello World!</h1>")
+})
+
+app.listen(port, hostname, () => {
+  console.log(`Hello Minh, I'm running server at http://${hostname}:${port}`)
+})
