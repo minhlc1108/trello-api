55d97b87a3deafcc876ed28005ea137af7b7712a
minhlc1108 <minh.lcm.2003@gmail.com>
2024-05-23 12:39:57 +0700

    create api get details board

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -6,6 +6,8 @@
  */
 import { slugify } from '~/utils/formatters'
 import { boardModel } from '~/models/boardModel'
+import ApiError from '~/utils/ApiError'
+import { StatusCodes } from 'http-status-codes'
 const creatNew = async (reqBody) => {
   try {
     const newBoard = {
@@ -26,6 +28,20 @@ const creatNew = async (reqBody) => {
   } catch (error) { throw error }
 }
 
+const getDetails = async (boardId) => {
+  try {
+
+    const board = await boardModel.getDetails(boardId)
+
+    if (!board) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found!')
+    }
+
+    return board
+  } catch (error) { throw error }
+}
+
 export const boardService = {
-  creatNew
+  creatNew,
+  getDetails
 }
